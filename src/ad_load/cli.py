d19f993ec4638c2720a64fc7b7299dd891f9900b
Vincent-Lin-UF <vincentlin1617@gmail.com>
2025-07-08 12:00:50 -0700

    Added Ctrl+C to stop chrome

--- src/ad_load/cli.py
+++ src/ad_load/cli.py
@@ -1,8 +1,24 @@
 import asyncio
+import signal
 from . import main
 
+def _make_shutdown_handler(loop):
+    def _handler():
+        for task in asyncio.all_tasks(loop):
+            task.cancel()
+    return _handler
+
 def main_entry():
-    asyncio.run(main.app(main.build_parser().parse_args()))
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGINT,  _make_shutdown_handler(loop))
+    loop.add_signal_handler(signal.SIGTERM, _make_shutdown_handler(loop))
+    
+    try:
+        asyncio.run(main.app(main.build_parser().parse_args()))
+    except asyncio.CancelledError:
+        pass
+    finally:
+        loop.close()
 
 if __name__ == "__main__":
     main_entry()
